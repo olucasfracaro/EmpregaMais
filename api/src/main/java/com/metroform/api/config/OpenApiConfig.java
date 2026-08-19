ddd5c7ac04e1e8146e02fa9702f78ac5d0607f6f
olucasfracaro <88164818+olucasfracaro@users.noreply.github.com>
2026-08-19 12:45:09 +0000

    pequenas configurações

---
+++ api/src/main/java/com/metroform/api/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.metroform.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(
+                        new Server()
+                                .url("https://localhost:8080")
+                                .description("Servidor local")
+                )
+                .addServersItem(
+                        new Server()
+                                .url("https://psychic-space-cod-4jj796xvj5fj544-8080.app.github.dev/")
+                                .description("Servidor do Codespace")
+                );
+    }
+}
